06f86c7dd555ab6e57b8fb397cc8c03a0ab481f3
rohobits <fostermartin67@gmail.com>
2025-11-09 15:36:47 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/everyday-essentials/" // <-- repo name with trailing slash
+  // Must be your repo name with leading AND trailing slashes:
+  base: "/everyday-essentials/"
 });
