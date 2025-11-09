ebcbef652e135947df32fee364b84a7c7deba8f7
rohobits <fostermartin67@gmail.com>
2025-11-09 15:07:19 -0700

    Create theme.ts

---
+++ src/lib/theme.ts
@@ -0,0 +1,6 @@
+export const BRAND = {
+  name: "Everyday Essentials",
+  phone: "(203) 710-8802",
+  addressLines: ["8795 Ralston Road", "Ste. 204B", "Arvada, CO 80002"],
+  email: "", // add later if desired
+};
